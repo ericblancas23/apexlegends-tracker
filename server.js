a9be95c3e4f66c99530b37d24208e92e9457d5c2
Eric Blancas <ericblancas23@gmail.com>
2019-08-21 19:18:08 -0700

    morgan and env setup

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const morgang = require('morgan');
+const morgan = require('morgan');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
@@ -8,10 +8,18 @@ const app = express();
 
 const port = process.env.PORT || 8000;
 
-app.get('/api/v1/:platform/:gamertag', (req, res) => {
-    res.send('hello');
-})
+// app.get('/api/v1/profile/:platform/:gamertag', (req, res) => {
+//     console.log(req.params.platform, req.params.gamertag);
+//     res.send('hello');
+// });
 
+//dev logging
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+//profile routes
+app.use('/api/v1/profile', require('./routes/profile'));
 
 app.listen(port, () => {
     console.log(`server is working: running in ${process.env.NODE_ENV} on ${port}`);
