7f2f7f26f3bfe17be9b8e6da04c6bf65d36ccada
Eric Blancas <ericblancas23@gmail.com>
2019-08-23 10:31:44 -0700

    error handling working

--- routes/profile.js
+++ routes/profile.js
@@ -7,9 +7,16 @@ router.get('/:platform/:gamertag', async (req, res) => {
     const headers = {
         'TRN-Api-Key': process.env.TRACKER_API_KEY
     }
-    const { platform, gamertag } = req.params; 
+    const { platform, gamertag } = req.params;
+    const response  = await fetch(`${process.env.TRACKER_API_KEY}/profile/
+    ${platform}/${gamertag}`, {
+        headers: headers
+    }); 
+    const data = await response.json();
+    res.json(data);
     } catch(err) {
-
+        console.error(err);
+        res.status(500).json({message: 'server error'});
     }
 });
 
