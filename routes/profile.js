4be40e908aa00f8c47d8cb6cb31ea265f20803ec
Eric Blancas <ericblancas23@gmail.com>
2019-08-28 19:10:59 -0700

    changes

--- routes/profile.js
+++ routes/profile.js
@@ -5,7 +5,7 @@ const fetch = require('node-fetch');
 router.get('/:platform/:gamertag', async (req, res) => {
     try {
     const headers = {
-        'TRN-Api-Key': process.env.TRACKER_API_URL
+        'TRN-Api-Key': process.env.TRACKER_API_KEY
     }
     const { platform, gamertag } = req.params;
     const response  = await fetch(`${process.env.TRACKER_API_URL}/profile/
